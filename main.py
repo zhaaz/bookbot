2e6e7569eead64e6d2f9c0da803700cf5b422ea8
zhaaz <a_wehner@outlook.de>
2025-11-25 15:33:24 +0100

    Weitere Bücher hinzugefügt und mit variablem Input getestet

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from stats import get_num_words, number_of_single_chars, sort_chars, report
+from stats import (
+    get_num_words, 
+    number_of_single_chars, 
+    sort_chars, 
+    report, 
+)
+import sys
 
 def main():
-    filepath = "books/frankenstein.txt"
+    if (len(sys.argv) < 2 or len(sys.argv) > 2):
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
     report(filepath)
 
    
-
-
 main()
